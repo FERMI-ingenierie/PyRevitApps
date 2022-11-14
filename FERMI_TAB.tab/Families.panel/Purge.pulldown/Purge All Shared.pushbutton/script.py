3dc8e560c21db9f73123d97f4cc4c98d9c4a527f
Jérôme PETITJEAN <j.petitjean@fermi.fr>
2022-11-14 16:37:28 +0100

    Update script.py

--- FERMI_TAB.tab/Families.panel/Purge.pulldown/Purge All Shared.pushbutton/script.py
+++ FERMI_TAB.tab/Families.panel/Purge.pulldown/Purge All Shared.pushbutton/script.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-
-
 __title__ = "Get and delete all shared parameters"
 __doc__ = """Version = 0.1
 Date    = November 2022
@@ -26,7 +24,6 @@ Author: SAS FERMI"""
 
 __author__ = "FERMI"
 __helpurl__ = "www.fermi.fr"
-__highlight__ = "new"                               # Button will have an orange dot + Description in Revit UI
 __min_revit_ver__ = 2022                            # Limit your Scripts to certain Revit versions if it's not compatible due to RevitAPI Changes.
 __max_revit_ver = 2023                             # Limit your Scripts to certain Revit versions if it's not compatible due to RevitAPI Changes.
 __context__     = ["doc-family"]
@@ -36,8 +33,11 @@ __context__     = ["doc-family"]
 # ╩╩ ╩╩  ╚═╝╩╚═ ╩ ╚═╝ Regular + Autodesk + pyRevit + Custom + .NET
 # =====================================================================================================================
 
-from Autodesk.Revit.DB import *
+from Autodesk.Revit.DB import Transaction
 from Snipets._Selection import get_all_shared_parameters
+
+from pyrevit.forms import ProgressBar
+from pyrevit import forms
 
 import clr
 clr.AddReference("System")
@@ -45,15 +45,28 @@ clr.AddReference("System")
 
 
 doc = __revit__.ActiveUIDocument.Document
-familyManager= doc.FamilyManager
-
 uidoc = __revit__.ActiveUIDocument
 
 
 if __name__ == '__main__':
-    with Transaction(doc, __title__) as t:
-        t.Start()
-        parameters = get_all_shared_parameters(doc)
-        for parameter in parameters:
-            doc.Delete(parameter.Id)
-        t.Commit()
+
+
+    selected_parameters = forms.select_family_parameters()
+    if selected_parameters:
+        pass
+
+
+
+    #
+    # parameters = get_all_shared_parameters(doc)
+    #
+    # with Transaction(doc) as transaction:
+    #     transaction.Start()
+    #
+    #     try:
+    #         for parameter in parameters:
+    #             doc.Delete(parameter.Id)
+    #     except (Exception)
+    #         Forms.
+    #
+    #     transaction.Commit()
