879bccdab9b6bb52d8cc94f98a0c848d24eff685
Jérôme PETITJEAN <j.petitjean@fermi.fr>
2022-11-15 12:49:20 +0100

    Update script.py

--- FERMI_TAB.tab/Project.panel/Update Elements.pulldown/Update altimétrie hosted elements.pushbutton/script.py
+++ FERMI_TAB.tab/Project.panel/Update Elements.pulldown/Update altimétrie hosted elements.pushbutton/script.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+
 __title__ = "Update altimetry of hosted elements"
 __author__ = "FERMI"
 __helpurl__ = "www.fermi.fr"
@@ -7,17 +8,14 @@ __min_revit_ver__ = 2022                            # Limit your Scripts to cert
 __max_revit_ver = 2023                             # Limit your Scripts to certain Revit versions if it's not compatible due to RevitAPI Changes.
 __context__     = ["doc-project"]
 
-import os, sys, math, datetime, time
+# import os, sys, math, datetime, time
 from Autodesk.Revit.DB import *
-from Autodesk.Revit.DB import Transaction, FilteredElementCollector
+# from pyrevit import revit, forms
+# from Lib.Snipets._Selection import get_selected_elements
 
-from pyrevit import revit, forms
-
-from Lib.Snipets._Selection import get_selected_elements
-
-import clr                                  # Common Language Runtime. Makes .NET libraries accessinble
-clr.AddReference("System")                  # Refference System.dll for import.
-from System.Collections.Generic import List # List<ElementType>() <- it's special type of list from .NET framework that RevitAPI requires
+# import clr                                  # Common Language Runtime. Makes .NET libraries accessinble
+# clr.AddReference("System")                  # Refference System.dll for import.
+# from System.Collections.Generic import List # List<ElementType>() <- it's special type of list from .NET framework that RevitAPI requires
 # List_example = List[ElementId]()          # use .Add() instead of append or put python list of ElementIds in parentesis.
 
 
