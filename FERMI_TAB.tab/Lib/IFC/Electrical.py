0e890c2ff36c728f2d136c4bc298a61ed2ae0ab7
Jérôme PETITJEAN <j.petitjean@fermi.fr>
2022-11-15 13:18:23 +0100

    up

---
+++ FERMI_TAB.tab/Lib/IFC/Electrical.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+class Pset_ElectricalDeviceCommon:
+    def __init__(self):
+        pass
+
+
+
+
+# RatedCurrent	P_BOUNDEDVALUE / IfcElectricCurrentMeasure
+# RatedVoltage	P_BOUNDEDVALUE / IfcElectricVoltageMeasure
+# NominalFrequencyRange	P_BOUNDEDVALUE / IfcFrequencyMeasure
+# PowerFactor	P_SINGLEVALUE / IfcNormalisedRatioMeasure
+# ConductorFunction	P_ENUMERATEDVALUE / IfcLabel / PEnum_ConductorFunctionEnum
+# NumberOfPoles	P_SINGLEVALUE / IfcInteger
+# HasProtectiveEarth	P_SINGLEVALUE / IfcBoolean
+# InsulationStandardClass	P_ENUMERATEDVALUE / IfcLabel / PEnum_InsulationStandardClass
+# IP_Code	P_SINGLEVALUE / IfcLabel
+# IK_Code	P_SINGLEVALUE / IfcLabel
--- FERMI_TAB.tab/Project.panel/Update Elements.pulldown/Update altimétrie hosted elements.pushbutton/script.py
+++ FERMI_TAB.tab/Project.panel/Update Elements.pulldown/Update altimétrie hosted elements.pushbutton/script.py
@@ -41,7 +41,7 @@ app = __revit__.Application
 if __name__ == '__main__':
     print ("go")
     select = FilteredElementCollector(doc) \
-        .OfCategory(BuiltInCategory.OST_LightingFixtures) \
+        .OfCategory(BuiltInCategory.OST_ElectricalEquipment) \
         .WhereElementIsNotElementType() \
         .ToElements()
     print ("selected")
