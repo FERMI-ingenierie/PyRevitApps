9d70959b20ad599515461060cc8222dca4d9504e
Jérôme PETITJEAN <j.petitjean@fermi.fr>
2022-11-15 12:45:54 +0100

    Update script.py

--- FERMI_TAB.tab/Project.panel/Update Elements.pulldown/Update altimétrie hosted elements.pushbutton/script.py
+++ FERMI_TAB.tab/Project.panel/Update Elements.pulldown/Update altimétrie hosted elements.pushbutton/script.py
@@ -1,47 +1,11 @@
 # -*- coding: utf-8 -*-
 __title__ = "Update altimetry of hosted elements"
-__doc__ = """Version = 0.1
-Date    = November 2022
-_____________________________________________________________________
-Description:
-
-Mise à jour de
-  - level
-  - altimetry parameter on hosted elements 
-_____________________________________________________________________
-How-to:
-
--> Click on the button
--> wait
-_____________________________________________________________________
-Last update:
-- [November 2022]
-_____________________________________________________________________
-To-Do:
-- all
-_____________________________________________________________________
-Author: SAS FERMI"""
-
-# ╔╗╔╔═╗╔╦╗╔═╗╔═╗
-# ║║║║ ║ ║ ║╣ ╚═╗
-# ╝╚╝╚═╝ ╩ ╚═╝╚═╝
-# progress barre : https://www.notion.so/Effective-Output-43baf34d2ca247ada8e040bcb86613a2
-
-
-
-
-# EXTRA: You can remove them.
 __author__ = "FERMI"
 __helpurl__ = "www.fermi.fr"
 __highlight__ = "new"                               # Button will have an orange dot + Description in Revit UI
 __min_revit_ver__ = 2022                            # Limit your Scripts to certain Revit versions if it's not compatible due to RevitAPI Changes.
 __max_revit_ver = 2023                             # Limit your Scripts to certain Revit versions if it's not compatible due to RevitAPI Changes.
 __context__     = ["doc-project"]
-
-# ╦╔╦╗╔═╗╔═╗╦═╗╔╦╗╔═╗
-# ║║║║╠═╝║ ║╠╦╝ ║ ╚═╗
-# ╩╩ ╩╩  ╚═╝╩╚═ ╩ ╚═╝ Regular + Autodesk + pyRevit + Custom + .NET
-# =====================================================================================================================
 
 import os, sys, math, datetime, time
 from Autodesk.Revit.DB import *
@@ -56,50 +20,36 @@ clr.AddReference("System")                  # Refference System.dll for import.
 from System.Collections.Generic import List # List<ElementType>() <- it's special type of list from .NET framework that RevitAPI requires
 # List_example = List[ElementId]()          # use .Add() instead of append or put python list of ElementIds in parentesis.
 
-# ╦  ╦╔═╗╦═╗╦╔═╗╔╗ ╦  ╔═╗╔═╗
-# ╚╗╔╝╠═╣╠╦╝║╠═╣╠╩╗║  ║╣ ╚═╗
-#  ╚╝ ╩ ╩╩╚═╩╩ ╩╚═╝╩═╝╚═╝╚═╝
-# ==================================================
-doc = __revit__.ActiveUIDocument.Document   # Document   class from RevitAPI that represents project. Used to Create, Delete, Modify and Query elements from the project.
-uidoc = __revit__.ActiveUIDocument          # UIDocument class from RevitAPI that represents Revit project opened in the Revit UI.
-app = __revit__.Application                 # Represents the Autodesk Revit Application, providing access to documents, options and other application wide data and settings.
-#PATH_SCRIPT = os.path.dirname(__file__)     # Absolute path to the folder where script is placed.
 
-# GLOBAL VARIABLES
 
-# - Place global variables here.
 
-# ╔═╗╦ ╦╔╗╔╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
-# ╠╣ ║ ║║║║║   ║ ║║ ║║║║╚═╗
-# ╚  ╚═╝╝╚╝╚═╝ ╩ ╩╚═╝╝╚╝╚═╝
-# =====================================================================================================================
 
-# - Place local functions here. If you might use any functions in other scripts, consider placing it in the lib folder.
+doc = __revit__.ActiveUIDocument.Document
+uidoc = __revit__.ActiveUIDocument
+app = __revit__.Application
 
-# ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗╔═╗
-# ║  ║  ╠═╣╚═╗╚═╗║╣ ╚═╗
-# ╚═╝╩═╝╩ ╩╚═╝╚═╝╚═╝╚═╝
-# =====================================================================================================================
 
-# - Place local classes here. If you might use any classes in other scripts, consider placing it in the lib folder.
 
-# ╔╦╗╔═╗╦╔╗╔
-# ║║║╠═╣║║║║
-# ╩ ╩╩ ╩╩╝╚╝
-# =====================================================================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
-    # START CODE HERE
-    # AVOID  placing Transaction inside of your loops! It will drastically reduce perfomance of your script.
-    #t = Transaction(doc,__title__)  # Transactions are context-like objects that guard any changes made to a Revit model.
+    select = FilteredElementCollector(doc) \
+        .OfCategory(BuiltInCategory.OST_ElectricalEquipment) \
+        .WhereElementIsNotElementType() \
+        .ToElements()
 
-    # You need to use t.Start() and t.Commit() to make changes to a Project.
-    #t.Start()  # <- Transaction Start
-
-    #- CHANGES TO REVIT PROJECT HERE
-
-    #t.Commit()  # <- Transaction End
-
-    # Notify user that script is complete.
-    print('-' * 50)
-    print('Script is finished.')
-    print('Template has been developed by FERMI _ Jérôme PETITJEAN.')
+    print (select)
