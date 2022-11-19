bd2b403b7d581af7ff79c9ba9659d6212339ec40
Jérôme PETITJEAN <j.petitjean@fermi.fr>
2022-11-19 12:40:36 +0100

    Update script.py

--- FERMI_TAB.tab/Project.panel/TEST.pushbutton/script.py
+++ FERMI_TAB.tab/Project.panel/TEST.pushbutton/script.py
@@ -63,8 +63,7 @@ if __name__ == '__main__':
     print Symbols
 
     for s in Symbols:
-        s.get_Parameter(BuiltInParameter.ALL_MODEL_DESCRIPTION).ToString()
-        print s
+        print s.get_Parameter(BuiltInParameter.ALL_MODEL_DESCRIPTION).ToString()
 
 
     # schedulable = SelectMEP_All_ElectricalElements.schedulable(elements)
