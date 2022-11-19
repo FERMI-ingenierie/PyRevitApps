f0b5a07fe7d59b6c73a0d3c291b21a35a5af92f8
Jérôme PETITJEAN <j.petitjean@fermi.fr>
2022-11-19 16:47:03 +0100

    Update script.py

--- FERMI_TAB.tab/Project.panel/TEST.pushbutton/script.py
+++ FERMI_TAB.tab/Project.panel/TEST.pushbutton/script.py
@@ -66,7 +66,7 @@ if __name__ == '__main__':
                 progressbar_counter += 1
                 pb.update_progress(progressbar_counter,maxvalue)
 
-                a = symbol.LookupParameter("test").AsValueString()
+                a = symbol.LookupParameter("test")
 
                 p = [symbol.LookupParameter(parameter).AsValueString() for parameter in parameters]
 
