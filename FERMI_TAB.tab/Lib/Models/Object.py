8793778a4e9cc262f706c216be3c8c096087a181
Jérôme PETITJEAN <j.petitjean@fermi.fr>
2022-11-19 17:45:36 +0100

    Update Object.py

--- FERMI_TAB.tab/Lib/Models/Object.py
+++ FERMI_TAB.tab/Lib/Models/Object.py
@@ -5,10 +5,10 @@ class ManufacturerProduct:
 
     def __init__(self):
         # self.key_Schedulable = 'SP_FER_SCH_Schedulable'
-        self.key_Fabricant = 'SP_FER_ID_Fabricant'
-        self.key_Fabricant_gamme = 'SP_FER_ID_Fabricant gamme'
-        self.key_Fabricant_reference = 'SP_FER_ID_Fabricant référence'
-        self.key_Product_URL = 'SP_FER_ID_Product URL'
+        self.key_Fabricant = "SP_FER_ID_Fabricant"
+        self.key_Fabricant_gamme = "SP_FER_ID_Fabricant gamme"
+        self.key_Fabricant_reference = "SP_FER_ID_Fabricant référence"
+        self.key_Product_URL = "SP_FER_ID_Product URL"
 
     @property
     def product_information(self):
@@ -21,10 +21,10 @@ class ManufacturerProduct:
 
     @property
     def parameters_names (self):
-        return (self.key_Fabricant,
-                self.key_Fabricant_gamme,
-                self.key_Fabricant_reference,
-                self.key_Product_URL)
+        return self.key_Fabricant,\
+               self.key_Fabricant_gamme,\
+               self.key_Fabricant_reference,\
+               self.key_Product_URL
 
     def set_data(self, key, value):
         setattr(self, key, value)
