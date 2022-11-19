1e3e1818ac45fc52c6184401d5e97ddbd516c296
Jérôme PETITJEAN <j.petitjean@fermi.fr>
2022-11-19 18:08:13 +0100

    Update Object.py

--- FERMI_TAB.tab/Lib/Models/Object.py
+++ FERMI_TAB.tab/Lib/Models/Object.py
@@ -12,9 +12,9 @@ class ManufacturerProduct:
 
     @property
     def product_information(self):
-        return "{}_{}_{}".format(getattr(self, "key_Fabricant"),
-                                 getattr(self, "key_Fabricant_gamme"),
-                                 getattr(self, "key_Fabricant_reference"))
+        return "{}_{}_{}".format(getattr(self, "key_Fabricant","Unknown"),
+                                 getattr(self, "key_Fabricant_gamme", "Unset"),
+                                 getattr(self, "key_Fabricant_reference", "Unset"))
     @property
     def product_url(self):
         return "{}".format(getattr(self, "key_Product_URL"))
@@ -27,70 +27,8 @@ class ManufacturerProduct:
                self.key_Product_URL
 
     def set_data(self, key, value):
-        setattr(self, key, value)
+        setattr(self, key, value,)
 
     @property
     def test(self):
-        return getattr(self, "key_Fabricant")
-
-
-
-
-
-    # class Outer(object):
-    #
-    #     def createInner(self):
-    #         return Outer.Inner(self)
-    #
-    #     class Inner(object):
-    #         def __init__(self, outer_instance):
-    #             self.outer_instance = outer_instance
-    #             self.outer_instance.somemethod()
-    #
-    #         def inner_method(self):
-    #             self.outer_instance.anothermethod()
-
-    # class TestClass(object):
-    #     def __init__(self, **kwargs):
-    #         for key, value in kwargs.items():  # items return list of dict
-    #             setattr(self, key, value)
-
-    # @property
-    # def klass(self):
-    #     return False
-    #
-    # @property
-    # def subklass(self):
-    #     return False
-    #
-    # @property
-    # def description(self):
-    #     return False
-    #
-    # @property
-    # def mark(self):
-    #     return False
-    #
-    # @property
-    # def gamme(self):
-    #     return False
-    #
-    # @property
-    # def reference(self):
-    #     return False
-    #
-    # @property
-    # def commande_code(self):
-    #     return "{}_{}_{}".format(self.mark, self.gamme, self.reference)
-    #
-
-
-
-
-
-
-
-
-
-
-
+        return getattr(self, "key_Fabricant", "No value")
